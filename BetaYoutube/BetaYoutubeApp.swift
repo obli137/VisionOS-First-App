7265623009a57164b9aeba05268660efde0949f0
obli <obli137@gmail.com>
2024-01-22 15:47:16 -0300

    Initial Commit

---
+++ BetaYoutube/BetaYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetaYoutubeApp.swift
+//  BetaYoutube
+//
+//  Created by Mariano Obligado on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetaYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetaYoutube/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BetaYoutube
+//
+//  Created by Mariano Obligado on 22/01/2024.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
